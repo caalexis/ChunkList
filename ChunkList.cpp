1ac2bac3aa7a6ac7422cc948b8d885937528e3f1
caalexis <calexis310@gmail.com>
2024-10-04 21:45:25 -0700

    Code outline

--- ChunkList.cpp
+++ ChunkList.cpp
@@ -4,4 +4,75 @@
 
 #include "ChunkList.h"
 
+// Default Constructor
+template<class T>
+ChunkList<T>::ChunkList() {
+
+}
+
+// Parameterized Constructor
+template<class T>
+ChunkList<T>::ChunkList(T arr[], int arrLen) {
+
+}
+
+// Destructor
+template<class T>
+ChunkList<T>::~ChunkList() {
+
+}
+
+// Append function
+template<class T>
+void ChunkList<T>::Append(T value) {
+
+}
+
+// Remove function
+template<class T>
+void ChunkList<T>::Remove(T value) {
+
+}
+
+// GetLength function
+template<class T>
+int ChunkList<T>::GetLength() {
+
+}
+
+// LoadFactor function
+template<class T>
+double ChunkList<T>::LoadFactor() {
+
+}
+
+// Contains function
+template<class T>
+bool ChunkList<T>::Contains(T value) {
+
+}
+
+// GetIndex function
+template<class T>
+T ChunkList<T>::GetIndex(int i) {
+
+}
+
+// ResetIterator function
+template<class T>
+void ChunkList<T>::ResetIterator() {
+
+}
+
+// GetNextItem function
+template<class T>
+T ChunkList<T>::GetNextItem() {
+
+}
+
+// IsEmpty function
+template<class T>
+bool ChunkList<T>::IsEmpty() {
+
+}
 
